5fdf572b09e275800b66592f89b5ced3d2c8a705
github username <github email address>
2019-05-22 16:24:19 -0700

    create boilerplate files for index.js, App, reducers

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// stateless functional component
+
+const App = () => {
+  return (
+    <div className="ui container">
+      App
+    </div>
+  );
+};
+
+export default App;
