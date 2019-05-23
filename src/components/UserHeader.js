ede55fb3531c845f6d0f6704fd8b85a4d614541b
github username <github email address>
2019-05-22 22:50:10 -0700

    creat UserHeader component, map out user.name for each post

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -10,12 +10,19 @@ class UserHeader extends React.Component {
   }
 
   render() {
-    return (
-      <div>
-        User Header
-      </div>
-    )
-  };
-}
+    const { user } = this.props;
 
-export default connect(null, { fetchUser })(UserHeader);
+      if (!user) {
+        return null;
+    }
+
+      return <div className="header">{user.name}</div>;
+    }
+  }
+
+  // ownProps can reference props from within the component itself (we're grabbing the userId from componentDidMount)
+const mapStateToProps = (state, ownProps) => {
+  return { user: state.users.find(user => user.id === ownProps.userId) };
+};
+
+export default connect(mapStateToProps, { fetchUser })(UserHeader);
